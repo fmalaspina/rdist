edf074d2810abfa586bcb0f35fe7ca61cdcaa4dc
Administrator <f_malaspina@hotmail.com>
2023-06-27 14:50:04 -0300

    work in progress

--- src/bin.rs
+++ src/bin.rs
@@ -3,11 +3,10 @@ use clap::Parser;
 use clap::Subcommand;
 use serde::{Deserialize, Serialize};
 use std::fs::File;
-
 use std::io::{Read, Write};
 use std::net::IpAddr;
 use std::net::TcpStream;
-
+use std::thread;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
@@ -19,35 +18,23 @@ struct Cli {
 enum Commands {
     /// Send file to target
     Copy {
-        /// Config file
+        /// Target node IP:PORT addresses to send the file to
         #[arg(short, long)]
-        config: String,
-        /// Target node IP address to send the file to
-        #[arg(short, long, value_parser = clap::value_parser!(IpAddr))]
-        destination: IpAddr,
+        ip_ports: Vec<String>,
         /// Source file to send
         #[arg(short, long)]
         source: String,
         /// Target file path
         #[arg(short, long)]
         target: String,
-        /// Target port number
-        #[arg(short, long, value_parser = clap::value_parser!(u16).range(1..))]
-        port: u16,
     },
     Run {
-        /// Config file
-        #[arg(short, long)]
-        config: String,
         /// Target node IP address to send the file to
         #[arg(short, long, value_parser = clap::value_parser!(IpAddr))]
-        destination: IpAddr,
+        ip_ports: Vec<String>,
         /// Remote command to run
         #[arg(short, long)]
         command: String,
-        /// Target port number
-        #[arg(short, long, value_parser = clap::value_parser!(u16).range(1..))]
-        port: u16,
     },
 }
 #[derive(Serialize, Deserialize)]
@@ -67,23 +54,18 @@ fn main() {
     //println!("{:?}", cli);
     match &cli.command {
         Some(Commands::Copy {
-            destination,
+            ip_ports,
             source,
             target,
-            port,
         }) => {
-            //println!("Send {destination:?},{source:?},{target:?},{port:?}");
-            let mut stream =
-                TcpStream::connect(format!("{}:{}", destination, port)).expect("Failed to connect");
-
             // Read the file into a byte vector
+
             let mut file_content = Vec::new();
             let mut file = File::open(source).expect("Error opening file");
             let file_content_size = file
                 .read_to_end(&mut file_content)
                 .expect("Error reading file");
 
-            // Create the message struct
             let message = Message {
                 data: file_content,
                 command: Command::Copy {
@@ -91,27 +73,33 @@ fn main() {
                 },
             };
 
-            // Serialize the message to bytes
             let message_bytes = bincode::serialize(&message).unwrap();
-
-            // Send message length
             let message_len = (message_bytes.len() as u32).to_be_bytes();
+
+            send_to_destinations(ip_ports, message_len, message_bytes);
+        }
+        Some(Commands::Run { ip_ports, command }) => {
+            println!("Send {ip_ports:?},{command:?}");
+        }
+        _ => (),
+    }
+}
+
+fn send_to_destinations(ip_ports: &Vec<String>, message_len: [u8; 4], message_bytes: Vec<u8>) {
+    for destination in ip_ports {
+        thread::spawn(move || {
+            let mut stream = TcpStream::connect(destination).expect("Failed to connect");
 
             stream
                 .write_all(&message_len)
                 .expect("Failed to send message length");
 
-            // Send the message
             stream
                 .write_all(&message_bytes)
                 .expect("Failed to send message");
+
             stream.flush().expect("Failed to flush stream");
-            println!(
-                "Message length sent: {}, file contents length sent: {}",
-                message_bytes.len(),
-                file_content_size
-            );
-            // Read the response from the server if needed
+
             let mut response_len_buffer = [0u8; 4];
             stream
                 .read_exact(&mut response_len_buffer)
@@ -122,23 +110,19 @@ fn main() {
             stream
                 .read_exact(&mut response_buffer)
                 .expect("Failed to read response data");
+
             // Convert the response buffer to a string
             let response_string = String::from_utf8_lossy(&response_buffer);
 
             // Print the response
             println!("Response: {}", response_string);
+
             stream
                 .shutdown(std::net::Shutdown::Write)
                 .expect("Failed to shutdown stream");
-        }
-        Some(Commands::Run {
-            destination,
-            command,
-            port,
-        }) => {
-            println!("Send {destination:?},{command:?},{port:?}");
-        }
-        _ => (),
+        })
+        .join()
+        .unwrap();
     }
 }
 
